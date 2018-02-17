c8b8961cbe7e7c26ead1ced54868ed0c161a63a7
Marius Steinbach <marius@steinbach.io>
2018-02-17 13:49:55 +0100

    Projektdateien hinzufügen.

---
+++ Cointer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cointer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
